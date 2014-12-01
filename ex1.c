c0115e270eabfe4958e8936bbe03eade944d7a26
gliubc <lbc_mail@163.com>
2014-12-01 11:36:11 +0800

    some

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	puts("Hello, world.");
+
+
+	puts("%%%@#$%@^@)_((^*$#%!Hello, world.");
+	puts("Hello, wor%ld.");
+	puts("Hello, worl%d.");
+	puts("Hello, worl\"d.");
+	puts("Hello, worl''d.");
+	return 0;
+}
+
